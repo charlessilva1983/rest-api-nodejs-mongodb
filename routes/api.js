b7983bcebc73834ceb2784932e091b8aecfa1b3f
charlessilva1983 <charles.silva1983@gmail.com>
2018-09-26 16:58:52 -0300

    Refactoring the end-point (api.js) with a more concise code pattern.

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const router = vertex.router()
 
 /*  This is a sample API route. */
 
-router.get('/:resource', (req, res) => {
+/*router.get('/:resource', (req, res) => {
 	res.json({
 		confirmation: 'success',
 		resource: req.params.resource,
@@ -20,8 +20,82 @@ router.get('/:resource/:id', (req, res) => {
 		id: req.params.id,
 		query: req.query // from the url query string
 	})
+})*/
+
+const players = [
+	{firstName:"Bromelia", class: "Dragonknight", selectedBuild: "2-Handed"},
+	{firstName:"Darknessheart", class: "Ranger", selectedBuild: "2-Handed"},
+	{firstName:"Harra", class: "Spellweaver", selectedBuild: "1-Handed"}
+]
+
+const guilds = [
+	{name: "RainningBlood", members: 10, createdat: "09/26/2018"},
+	{name: "FairyTail", members: 50, createdat: "09/26/2018"}
+]
+
+const db = {
+	guild: guilds,
+	player: players
+}
+
+router.get('/:resource', (req, res) => {
+	const resource = req.params.resource;
+
+	const data = db[resource];
+
+	if (data == null){
+		res.json({
+			confirmation: 'fail',
+			message: 'Invalid request!'
+		})
+
+		return
+	}
+
+	res.json({
+		confirmation: 'sucess',
+		data: data
+	})
+
+	// if(resource == 'player'){
+	// 	res.json({
+	// 		confirmation: 'sucess',
+	// 		data: players
+	// 	})
+
+	// 	return
+	// }
+
+	// if(resource == 'guild'){
+	// 	res.json({
+	// 		confirmation: 'sucess',
+	// 		data: guilds
+	// 	})
+
+	// 	return
+	// }
+
+	// res.json({
+	// 	confirmation: 'fail',
+	// 	message: 'Invalid request!'
+	// })
+
 })
 
+/* 	router.get('/test', (req, res) => {
 
+		res.json({
+			confirmation: 'sucess',
+			data: 'this is a test endpoint'
+		})
+	})
+
+	router.get('/guild', (req, res) => {
+
+		res.json({
+			confirmation: 'sucess',
+			data: guilds
+		})
+	}) */
 
 module.exports = router
